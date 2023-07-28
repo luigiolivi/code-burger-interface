033e6ad232bc17781bbbdb16e3f74b2bbad4f3f6
luigiolivi <eterowiczluigi@gmail.com>
2023-07-27 21:30:51 -0300

    eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 import Login from './containers/Login'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
     <Login />
   </>
-);
-
+)
