7bc0292e1157584319ecf369ff4041cf6f19afb7
luigiolivi <eterowiczluigi@gmail.com>
2023-07-28 23:03:20 -0300

    login structure

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
+import {
+  Container,
+  Background,
+  ContainerItems,
+  P,
+  Input,
+  Button,
+  SignInLink
+} from './styles'
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <Background />
+      <ContainerItems>
+        <img />
+        <h1>Login</h1>
+
+        <P>Email</P>
+        <Input />
+        <P>Senha</P>
+        <Input />
+
+        <Button>Sign In</Button>
+        <SignInLink>
+          Não possui conta? <a>Sign Up</a>
+        </SignInLink>
+      </ContainerItems>
+    </Container>
   )
 }
 
