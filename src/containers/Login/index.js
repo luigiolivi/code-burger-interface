14e90e690cdbfbb5072611109ebf2e1e3a2884fe
luigiolivi <eterowiczluigi@gmail.com>
2023-08-01 20:53:47 -0300

    axios post sessions

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,7 +1,11 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as Yup from 'yup'
 
 import LoginImg from '../../assets/login-image.png'
 import Logo from '../../assets/logo.png'
+import api from '../../services/api'
 import {
   Container,
   LoginImage,
@@ -9,10 +13,35 @@ import {
   Label,
   Input,
   Button,
-  SignInLink
+  SignInLink,
+  Error
 } from './styles'
 
 function Login() {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Digite um email válido')
+      .required('O email é obrigatório'),
+    password: Yup.string()
+      .required('A senha é obrigatória')
+      .min(6, 'A senha deve conter 6 caracteres')
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({ resolver: yupResolver(schema) })
+
+  const onSubmit = async clientData => {
+    const response = await api.post('sessions', {
+      email: clientData.email,
+      password: clientData.password
+    })
+
+    console.log(response)
+  }
+
   return (
     <Container>
       <LoginImage src={LoginImg} alt="logo-image" />
@@ -20,12 +49,26 @@ function Login() {
         <img src={Logo} alt="logo" />
         <h1>Login</h1>
 
-        <Label>Email</Label>
-        <Input />
-        <Label>Senha</Label>
-        <Input />
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <Label>Email</Label>
+          <Input
+            type="email"
+            {...register('email')}
+            error={errors.email?.message}
+          />
+          <Error>{errors.email?.message}</Error>
 
-        <Button>Sign In</Button>
+          <Label>Senha</Label>
+          <Input
+            type="password"
+            {...register('password')}
+            error={errors.password?.message}
+          />
+          <Error>{errors.password?.message}</Error>
+
+          <Button type="submit">Sign In</Button>
+        </form>
+
         <SignInLink>
           Não possui conta? <a>Sign Up</a>
         </SignInLink>
--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -3,26 +3,109 @@ import styled from 'styled-components'
 import Background from '../../assets/background.png'
 
 export const Container = styled.div`
-  height: 100%;
-  min-height: 100vh;
-  width: 100%;
-  min-width: 100vw;
-
+  height: 100vh;
+  width: 100vw;
   background: url('${Background}');
-
   display: flex;
   justify-content: center;
   align-items: center;
 `
 
-export const LoginImage = styled.img``
+export const LoginImage = styled.img`
+  height: 80%;
+`
 
-export const ContainerItems = styled.div``
+export const ContainerItems = styled.div`
+  border-radius: 0 10px 10px 0;
+  background: #373737;
+  height: 80%;
+  padding: 25px 75px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 
-export const Label = styled.p``
+  h1 {
+    color: #fff;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    text-align: center;
+    margin-top: 40px;
+  }
 
-export const Input = styled.input``
+  img {
+    width: 260px;
+    height: 94px;
+    margin: 0 auto;
+  }
 
-export const Button = styled.button``
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+`
 
-export const SignInLink = styled.p``
+export const Label = styled.p`
+  color: #fff;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin: 25px 0 3px;
+`
+
+export const Input = styled.input`
+  border-radius: 5px;
+  background: #fff;
+  box-shadow: 3px 3px 10px 0px rgba(74, 144, 226, 0.19);
+  border: ${props => (props.error ? '2px solid #CC1717' : 'none')};
+  height: 40px;
+  width: 390px;
+  padding-left: 10px;
+`
+
+export const Button = styled.button`
+  width: 183px;
+  height: 36px;
+  color: #eee;
+  text-align: center;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  background: #9758a6;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  margin: 45px 0 15px;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.6;
+  }
+`
+
+export const SignInLink = styled.p`
+  color: #fff;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+
+  a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
+
+export const Error = styled.p`
+  color: #cc1717;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`
