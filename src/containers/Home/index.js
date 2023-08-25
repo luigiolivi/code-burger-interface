4a0ccc387e4db2d2050e890623de61e620092644
luigiolivi <eterowiczluigi@gmail.com>
2023-08-24 22:50:58 -0300

    carousel component

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import HomeImage from '../../assets/home-image.png'
+import CategoryCarousel from '../../components/CategoryCarousel'
+import { Container, HomeImg } from './styles'
+
 function Home() {
-  return <div>Home</div>
+  return (
+    <Container>
+      <HomeImg src={HomeImage} alt="logo-image" />
+      <CategoryCarousel />
+    </Container>
+  )
 }
 
 export default Home
