9bb61fcaf0ce55e9a26ffd5840d1b85431441d45
luigiolivi <eterowiczluigi@gmail.com>
2023-08-25 20:31:50 -0300

    carousel styling

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -1,23 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Carousel from 'react-elastic-carousel'
 
 import Categories from '../../assets/categories.png'
 import api from '../../services/api'
-import { Container, CategoryImg } from './styles'
+import { Container, CategoryImg, ContainerItems, Image, Button } from './styles'
 
 function CategoryCarousel() {
+  const [categories, setCategories] = useState([])
+
   useEffect(() => {
     async function loadCategories() {
-      const response = await api.get('categories')
+      const { data } = await api.get('categories')
 
-      console.log(response)
+      setCategories(data)
     }
 
     loadCategories()
   }, [])
 
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 400, itemsToShow: 2 },
+    { width: 600, itemsToShow: 3 },
+    { width: 900, itemsToShow: 4 },
+    { width: 1300, itemsToShow: 5 }
+  ]
+
   return (
     <Container>
       <CategoryImg src={Categories} alt="categories-logo" />
+
+      <Carousel
+        itemsToShow={5}
+        style={{ width: '90%' }}
+        breakPoints={breakPoints}
+      >
+        {categories &&
+          categories.map(category => (
+            <ContainerItems key={category.id}>
+              <Image src={category.url} alt="category-image" />
+              <Button>{category.name}</Button>
+            </ContainerItems>
+          ))}
+      </Carousel>
     </Container>
   )
 }
